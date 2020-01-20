87814edb5a8b2e4ae39426e7340bfcf9339c9c92
Cody Bolleboom <1031226@idcollege.nl>
2020-01-20 20:39:36 +0100

    Made the tapping work on mobile

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -9,29 +9,65 @@ public class GameHandler : MonoBehaviour
 
     public float pullSpeed;
 
+    public float timer = 0;
+
     // Start is called before the first frame update
     void Start()
     { 
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonUp(0))
+        this.timer += Time.deltaTime;
+
+        if (linePositions[0].x < 0 && linePositions[1].x > 0)
         {
-            if (linePositions[0].x > -8) {
-                linePositions[0] = new Vector2(linePositions[0].x - this.pullSpeed, 0);
+            if (Input.touchCount > 0)
+            {
+                foreach (Touch touch in Input.touches)
+                {
+                    Vector2 location = new Vector2(touch.position.x, touch.position.y);
+
+                    if (location.x < (Screen.width / 2))
+                    {
+                        if (linePositions[0].x > -8)
+                        {
+                            linePositions[0] = new Vector2(linePositions[0].x - this.pullSpeed, 0);
+                        }
+
+                        linePositions[1] = new Vector2(linePositions[1].x - this.pullSpeed, 0);
+                    }
+                    else if (location.x > (Screen.width / 2))
+                    {
+                        if (linePositions[1].x < 8)
+                        {
+                            linePositions[1] = new Vector2(linePositions[1].x + this.pullSpeed, 0);
+                        }
+                        linePositions[0] = new Vector2(linePositions[0].x + this.pullSpeed, 0);
+                    }
+                }
             }
 
-            linePositions[1] = new Vector2(linePositions[1].x - this.pullSpeed, 0);
-        }
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (linePositions[0].x > -8)
+                {
+                    linePositions[0] = new Vector2(linePositions[0].x - this.pullSpeed, 0);
+                }
 
-        if (Input.GetMouseButtonUp(1))
-        {
-            if (linePositions[1].x < 8) {
-                linePositions[1] = new Vector2(linePositions[1].x + this.pullSpeed, 0);
+                linePositions[1] = new Vector2(linePositions[1].x - this.pullSpeed, 0);
             }
-            linePositions[0] = new Vector2(linePositions[0].x + this.pullSpeed, 0);
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                if (linePositions[1].x < 8)
+                {
+                    linePositions[1] = new Vector2(linePositions[1].x + this.pullSpeed, 0);
+                }
+                linePositions[0] = new Vector2(linePositions[0].x + this.pullSpeed, 0);
+            }
         }
     }
 
