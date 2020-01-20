4d30cf31300e24d3db48ae7a8a7668d5a39fa9c8
Cody Bolleboom <1031226@idcollege.nl>
2020-01-20 19:26:26 +0100

    Added map and rope

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public List<Vector2> linePositions;
+
+    public float pullSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (linePositions[0].x > -8) {
+                linePositions[0] = new Vector2(linePositions[0].x - this.pullSpeed, 0);
+            }
+
+            linePositions[1] = new Vector2(linePositions[1].x - this.pullSpeed, 0);
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            if (linePositions[1].x < 8) {
+                linePositions[1] = new Vector2(linePositions[1].x + this.pullSpeed, 0);
+            }
+            linePositions[0] = new Vector2(linePositions[0].x + this.pullSpeed, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.drawLine();
+    }
+
+    /**
+     * Draw line
+     */
+    public void drawLine()
+    {
+        for (int i = 0; i < this.linePositions.Count; i++)
+        {
+            this.lineRenderer.SetPosition(i, linePositions[i]);
+        }
+    }
+}
