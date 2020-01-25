32e151593eb5f745817551a0142f1eb87e4706cf
Cody Bolleboom <1031226@idcollege.nl>
2020-01-25 17:23:00 +0100

    cleaned code

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -4,6 +4,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/**
+ * @var LineRenderer lineRenderer is the line it self
+ * @var List<Vector2> linePositions contains the anchor points of the line
+ * @var List<GameObject> Players contains the player GameObjects (that contains the player sprite)
+ * 
+ * @var GameObject menu used to access the ui elements in the menu
+ * 
+ * @var float timer contains the time after start
+ * @var Text timerHolder this is the place where the variable timer is displayed
+ * 
+ * @var float countDown this is the countdown time
+ * @var GameObject countDownText contains the background image of the start countdown
+ * @var Text countDownHolder place where the start countdown is displayed
+ * 
+ * @var float pullSpeed how far a tap/click moves the players
+ */
 public class GameHandler : MonoBehaviour
 {
     public LineRenderer lineRenderer;
@@ -12,54 +28,43 @@ public class GameHandler : MonoBehaviour
 
     public GameObject menu;
 
+    public float timer = 0;
     public Text timerHolder;
 
+    public float countDown = 3;
     public GameObject countDownText;
     public Text countDownHolder;
-    public float countDown = 3;
 
     public float pullSpeed;
 
-    public float timer = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        this.displayCountDown();
-    }
-
-    private void displayCountDown() {
-        this.countDown -= Time.deltaTime;
-
-        if (this.countDown <= -1)
-        {
-            countDownText.SetActive(false);
-            this.countDownHolder.text = "";
-
-            this.countDown = -1;
-        }
-        else if (Math.Ceiling(this.countDown) <= 0)
-        {
-            this.countDownHolder.text = "GO";
-        }
-        else
-        {
-            this.countDownHolder.text = Math.Ceiling(this.countDown).ToString();
-        }
-    }
-
-    // Update is called once per frame
+    /**
+     * Does this every frame
+     */
     void Update()
     {
         if (Math.Ceiling(this.countDown) <= 0)
         {
+            /**
+             * does this when countdown says go
+             */
+
             if (this.countDown >= -1)
             {
-                this.displayCountDown();
+                /**
+                 * hide countdown
+                 */
+                this.countDownHolder.text = this.displayCountDown();
             }
 
-            if ((linePositions[0].x < 0 && linePositions[1].x > 0) && this.timer < 100)
+            if ((this.linePositions[0].x < 0 && this.linePositions[1].x > 0) && this.timer < 100)
             {
+                /**
+                 * No one has won or ran out of time
+                 */
+
+                /**
+                 * add to current time
+                 */
                 this.timer += Time.deltaTime;
 
                 /**
@@ -67,26 +72,58 @@ public class GameHandler : MonoBehaviour
                  */
                 if (Input.touchCount > 0)
                 {
+                    /**
+                     * when a player taps on screen
+                     */
+
+
+                    /**
+                     * loops through all the taps and see on what side it's on
+                     */
                     foreach (Touch touch in Input.touches)
                     {
+                        /**
+                         * @var Vector2 location get tap position
+                         */
                         Vector2 location = new Vector2(touch.position.x, touch.position.y);
 
                         if (location.x < (Screen.width / 2))
                         {
-                            if (linePositions[0].x > -8)
+                            /**
+                             * tap on left side
+                             */
+
+                            if (this.linePositions[0].x > -8)
                             {
-                                linePositions[0] = new Vector2(linePositions[0].x - this.pullSpeed, 0);
+                                /**
+                                 * moves player when it's not on the map border
+                                 */
+                                this.linePositions[0] = new Vector2(this.linePositions[0].x - this.pullSpeed, 0);
                             }
 
-                            linePositions[1] = new Vector2(linePositions[1].x - this.pullSpeed, 0);
+                            /**
+                             * moves opponent
+                             */
+                            this.linePositions[1] = new Vector2(this.linePositions[1].x - this.pullSpeed, 0);
                         }
                         else if (location.x > (Screen.width / 2))
                         {
-                            if (linePositions[1].x < 8)
+                            /**
+                             * tap on right side
+                             */
+
+                            if (this.linePositions[1].x < 8)
                             {
-                                linePositions[1] = new Vector2(linePositions[1].x + this.pullSpeed, 0);
+                                /**
+                                 * moves player when it's not on the map border
+                                 */
+                                this.linePositions[1] = new Vector2(this.linePositions[1].x + this.pullSpeed, 0);
                             }
-                            linePositions[0] = new Vector2(linePositions[0].x + this.pullSpeed, 0);
+
+                            /**
+                             * moves opponent
+                             */
+                            this.linePositions[0] = new Vector2(this.linePositions[0].x + this.pullSpeed, 0);
                         }
                     }
                 }
@@ -100,20 +137,43 @@ public class GameHandler : MonoBehaviour
                  */
                 if (Input.GetMouseButtonUp(0))
                 {
+                    /**
+                     * on left click
+                     */
+
                     if (linePositions[0].x > -8)
                     {
+                        /**
+                         * moves player when it's not on the map border
+                         */
+
                         linePositions[0] = new Vector2(linePositions[0].x - this.pullSpeed, 0);
                     }
 
+                    /**
+                     * moves opponent
+                     */
                     linePositions[1] = new Vector2(linePositions[1].x - this.pullSpeed, 0);
                 }
 
                 if (Input.GetMouseButtonUp(1))
                 {
+                    /**
+                     * on right click
+                     */
+
                     if (linePositions[1].x < 8)
                     {
+                        /**
+                         * moves player when it's not on the map border
+                         */
+
                         linePositions[1] = new Vector2(linePositions[1].x + this.pullSpeed, 0);
                     }
+
+                    /**
+                     * moves opponent
+                     */
                     linePositions[0] = new Vector2(linePositions[0].x + this.pullSpeed, 0);
                 }
                 /**
@@ -122,6 +182,14 @@ public class GameHandler : MonoBehaviour
             }
             else 
             {
+                /**
+                 * when a player reached the center of the map (when someone lost) 
+                 * or when the timer hit 100 sec (ran out of time)
+                 */
+
+                /**
+                 * @var Text gameOverInfo get text box with the winner information
+                 */
                 Text gameOverInfo = null;
                 for (int i = 0; i < this.menu.transform.childCount; i++) {
                     if (this.menu.transform.GetChild(i).name == "GameOverInfo") {
@@ -129,44 +197,122 @@ public class GameHandler : MonoBehaviour
                     }
                 }
 
-                if (this.timer >= 100)
-                {
-                    this.timerHolder.text = String.Format("{0:0.0}", 100);
-                    gameOverInfo.text = "Ran out of time";
-                }
-                else if (linePositions[0].x >= 0)
-                {
-                    gameOverInfo.text = "Blue wins";
-                }
-                else
-                {
-                    gameOverInfo.text = "Red wins";
-                }
+                /**
+                 * sets the gameover text
+                 */
+                gameOverInfo.text = this.gameOverText();
 
+                /**
+                 * shows the menu
+                 */
                 this.menu.SetActive(true);
             }
         } else {
-            this.displayCountDown();
+            /**
+             * shows remaining time before start
+             */
+            this.countDownHolder.text = this.displayCountDown();
         }
     }
 
-    // Update is called once per frame
+    /**
+     * edits UI elements after frame
+     */
     void FixedUpdate()
     {
+        /**
+         * sets the current time
+         */
         this.timerHolder.text = String.Format("{0:0.00}", this.timer);
 
         this.drawLine();
     }
 
     /**
-     * Draw line
+     * gets the gameover text
+     */
+    public string gameOverText()
+    {
+        if (this.timer >= 100)
+        {
+            /**
+             * when the players ran out of time
+             */
+
+            this.timerHolder.text = String.Format("{0:0.0}", 100);
+            return "Ran out of time";
+        }
+        else if (linePositions[0].x >= 0)
+        {
+            /**
+             * when the right player wins
+             */
+
+            return "Blue wins";
+        }
+        else
+        {
+            /**
+             * when the left player wins
+             */
+
+            return "Red wins";
+        }
+    }
+
+    /**
+     * Draws line and moves players
      */
     public void drawLine()
     {
         for (int i = 0; i < this.linePositions.Count; i++)
         {
-            this.lineRenderer.SetPosition(i, linePositions[i]);
-            Players[i].transform.position = new Vector2(linePositions[i].x, Players[i].transform.position.y);
+            /**
+             * set new line anchor points
+             */
+            this.lineRenderer.SetPosition(i, this.linePositions[i]);
+
+            /**
+             * set new position of player sprites
+             */
+            this.Players[i].transform.position = new Vector2(this.linePositions[i].x, this.Players[i].transform.position.y);
+        }
+    }
+
+    /**
+ * used for displaying the countdown at the start of the game
+ * 
+ * @returns string this shows the time remaining
+ */
+    private string displayCountDown()
+    {
+        /**
+         * adds the time between the frames
+         */
+        this.countDown -= Time.deltaTime;
+
+        if (this.countDown <= -1)
+        {
+            /**
+            * disable when countdown is over
+            */
+            this.countDownText.SetActive(false);
+            this.countDown = -1;
+            return "";
+        }
+        else if (Math.Ceiling(this.countDown) <= 0)
+        {
+            /**
+            * tells the player when to start
+            */
+            return "GO";
+        }
+        else
+        {
+            /**
+            * shows time remaining
+            */
+            return Math.Ceiling(this.countDown).ToString();
         }
     }
 }
--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneHandler : MonoBehaviour
 {
+    /**
+     * used for button navigatoin
+     */
     public void toScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
